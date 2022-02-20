66d00af71226a72e8c664ca54f951fc8521c1e30
Yi <yitashasimida@gmail.com>
2022-02-20 15:20:15 +1100

    Amplifying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Authenticator } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!!</h1>
+        <h1>We now have Auth!</h1>
       </header>
-      
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default Authenticator(App);
+export default withAuthenticator(App);
