67e1fc24324f93ca2cc9c0776f1cb8b67a106399
Yi <yitashasimida@gmail.com>
2022-02-20 17:23:28 +1100

    ui react@1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!!</h1>
+        <h1>We now have Auth Yes!!</h1>
       </header>
+
       <AmplifySignOut />
     </div>
   );
